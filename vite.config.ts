b4402c4cd7f556967d769bbd6959c294bb72ecf9
lby <icesunex@hotmail.com>
2022-02-11 15:27:08 +0800

    set config alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    open: true,
+  },
+  resolve:{
+    alias:{
+      "@rf": "/src"
+    }
+  },
   plugins: [react()],
 });
