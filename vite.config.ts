831b7ada0cc0ef7a3f8757dbf805a8ee89c4f9c1
XU Cong <lavalselike@gmail.com>
2022-02-13 23:12:42 +0900

    update lint

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  server: {
     open: true,
   },
-  resolve:{
-    alias:{
-      "@rf": "/src"
-    }
+  resolve: {
+    alias: {
+      "@rf": "/src",
+    },
   },
   plugins: [react()],
 });
