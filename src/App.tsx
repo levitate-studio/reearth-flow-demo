ec024b993a032666f5bf0f9e8e6248eb1c9adb63
XU Cong <lavalselike@gmail.com>
2022-02-27 15:59:00 +0900

    add a cesium playground

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Router } from "@reach/router";
 
 import Example_1 from "./components/examples/1";
 import Example_2 from "./components/examples/2";
-import Example_3 from "./components/examples/3";
+import Example_5 from "./components/examples/5";
 import { DnDFlow as Example_a } from "./components/examples/a";
 import { ExampleB as Example_b } from "./components/examples/b";
 import { ExampleC as Example_c } from "./components/examples/c";
@@ -18,7 +18,7 @@ function App() {
         <Examples path="/examples" />
         <Example_1 path="/example_1" />
         <Example_2 path="/example_2" />
-        <Example_3 path="/example_3" />
+        <Example_5 path="/example_5" />
         <Example_a path="/example_a" />
         <Example_b path="/example_b" />
         <Example_c path="/example_c" />
