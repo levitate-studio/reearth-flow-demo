3bc745532ae940d0c352f8f076a5d409c451c29d
lby <icesunex@hotmail.com>
2022-03-14 15:52:28 +0800

    add function for common object package

--- src/components/LvtFlow/Core/CommFuc.ts
+++ src/components/LvtFlow/Core/CommFuc.ts
@@ -1,3 +1,6 @@
+// =======================================
+// GUID
+// =======================================
 const S4 = () => {
   return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 };
@@ -5,6 +8,9 @@ export const createGUID = () => {
   return `${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`;
 };
 
+// =======================================
+// Auto increasing ID
+// =======================================
 const createIdFucCreator = () => {
   let id = 1;
   return {
@@ -19,14 +25,83 @@ const createIdFucCreator = () => {
 
 export const idCreator = createIdFucCreator();
 
-export const spreadData = (data: any) => {
-  return Array.isArray(data) ? data : [data];
-};
-
+// =======================================
+// Update Node
+// =======================================
 export const updateNode = (node: any, output: string, inputs: string[]) => {
   const inputValues: any = [];
   inputs.forEach((input) => {
     inputValues.push(node.getPortInByName(input)?.getValue());
   });
   node.getPortOutByName(output)?.setValue(node.rule.apply(null, inputValues));
+};
+
+// =======================================
+// Spread
+// =======================================
+export const spreadData = (data: any) => {
+  return Array.isArray(data) || data === undefined ? data : [data];
+};
+
+// =======================================
+// Package Spread Composite Value
+// =======================================
+export const packageSpreadCompositeValues = (
+  props: any,
+  packageType: "object" | "array",
+  objKey: string
+) => {
+  const spreadProps: any = {};
+  const spreadLength: number[] = [];
+  Object.keys(props).forEach((propName) => {
+    if (props[propName]) {
+      spreadProps[propName] = spreadData(props[propName]);
+      spreadLength.push(spreadProps[propName].length);
+    }
+  });
+  const maxLength = Math.max(...spreadLength);
+  const outputSpread: any = [];
+  for (let i = 0; i < maxLength; i += 1) {
+    let ele: any;
+    if (packageType === "object") {
+      ele = {};
+      Object.keys(spreadProps).forEach((propName: string) => {
+        ele[propName] = spreadProps[propName][i % spreadProps[propName].length];
+      });
+    } else if (packageType === "array") {
+      ele = [];
+      Object.keys(spreadProps).forEach((propName: string) => {
+        ele.push(spreadProps[propName][i % spreadProps[propName].length]);
+      });
+    }
+    //
+    const current: any = {};
+    current[objKey] = ele;
+    outputSpread.push(current);
+  }
+  return outputSpread;
+};
+
+// =======================================
+// Package Spread Value
+// =======================================
+export const packageSpreadValue = (props: any) => {
+  const spreadProps: any = {};
+  const spreadLength: number[] = [];
+  Object.keys(props).forEach((propName) => {
+    if (props[propName] !== undefined) {
+      spreadProps[propName] = spreadData(props[propName]);
+      spreadLength.push(spreadProps[propName].length);
+    }
+  });
+  const maxLength = Math.max(...spreadLength);
+  const outputSpread: any = [];
+  for (let i = 0; i < maxLength; i += 1) {
+    const ele: any = {};
+    Object.keys(spreadProps).forEach((propName: string) => {
+      ele[propName] = spreadProps[propName][i % spreadProps[propName].length];
+    });
+    outputSpread.push(ele);
+  }
+  return outputSpread;
 };
