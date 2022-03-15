b44e5c577ff0f4b2da2c9adfe93d0f0fb47405cc
lby <icesunex@hotmail.com>
2022-03-15 18:36:46 +0800

    remove node RGBA

--- src/components/LvtFlow/Nodes/Ops/Color/index.ts
+++ src/components/LvtFlow/Nodes/Ops/Color/index.ts
@@ -1,8 +1,7 @@
 import HexToRGBA from "./HexToRGBA";
 import HexToRGBAObj from "./HexToRGBAObj";
-import RGBA from "./RGBA";
 
 export default {
   _id: "Color",
-  children: [RGBA, HexToRGBA, HexToRGBAObj],
+  children: [HexToRGBA, HexToRGBAObj],
 };
