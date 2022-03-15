9c867c8ebc2e759d69884f72c75840eb0ff8886f
lby <icesunex@hotmail.com>
2022-03-15 19:42:44 +0800

    Optimize react component

--- src/components/Editor/NodesPanel/index.tsx
+++ src/components/Editor/NodesPanel/index.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 import * as Nodes from "../../LvtFlow/Nodes";
 
 import "./df-nodes-panel.css";
@@ -8,34 +10,39 @@ const NodesPanel = () => {
     event.dataTransfer.effectAllowed = "move";
   };
 
-  const l = (element: any, prefix: any) => {
-    if (typeof element === "object" && typeof element[0] !== "string") {
+  const nodeList = useMemo(() => {
+    const l = (element: any, prefix: any) => {
+      if (typeof element === "object" && typeof element[0] !== "string") {
+        console.log(element);
+        return Object.keys(element).map((key: string) => (
+          <div className="df-block-group" key={key}>
+            <div className={`df-block-group-title catcolor${prefix}-${key}`}>
+              {key}
+            </div>
+            <div className="df-block-group-wrapper">
+              {l(element[key], `${prefix}-${key}`)}
+            </div>
+          </div>
+        ));
+      }
       return Object.keys(element).map((key: string) => (
-        <div className="df-block-group" key={key}>
-          <div className={`df-block-group-title catcolor${prefix}-${key}`}>
-            {key}
-          </div>
-          <div className="df-block-group-wrapper">
-            {l(element[key], `${prefix}-${key}`)}
-          </div>
+        <div
+          key={Nodes.nodeDefs[element[key]].nodeId}
+          className={`node-ref catcolor-${
+            Nodes.nodeDefs[element[key]].category
+          }`}
+          onDragStart={(event) =>
+            onDragStart(event, Nodes.nodeDefs[element[key]].nodeId)
+          }
+          draggable
+        >
+          {Nodes.nodeDefs[element[key]].ui.title}
         </div>
       ));
-    }
-    return Object.keys(element).map((key: string) => (
-      <div
-        key={Nodes.nodeDefs[element[key]].nodeId}
-        className={`node-ref catcolor-${Nodes.nodeDefs[element[key]].category}`}
-        onDragStart={(event) =>
-          onDragStart(event, Nodes.nodeDefs[element[key]].nodeId)
-        }
-        draggable
-      >
-        {Nodes.nodeDefs[element[key]].ui.title}
-      </div>
-    ));
-  };
+    };
 
-  const nodeList = l(Nodes.nodeTree, "");
+    return l(Nodes.nodeTree, "");
+  }, []);
 
   return (
     <div className="df-nodes-panel">
