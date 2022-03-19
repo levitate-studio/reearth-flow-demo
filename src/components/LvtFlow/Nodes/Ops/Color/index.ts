6f20c31b111c34932b5badf819d3c1bd03a3f000
lby <icesunex@hotmail.com>
2022-03-19 14:29:57 +0800

    remove node HexToRGBAObj

--- src/components/LvtFlow/Nodes/Ops/Color/index.ts
+++ src/components/LvtFlow/Nodes/Ops/Color/index.ts
@@ -1,8 +1,7 @@
 import HexToRGBA from "./HexToRGBA";
-import HexToRGBAObj from "./HexToRGBAObj";
 import HSLAToRGBA from "./HSLAToRGBA";
 
 export default {
   _id: "Color",
-  children: [HexToRGBA, HSLAToRGBA, HexToRGBAObj],
+  children: [HexToRGBA, HSLAToRGBA],
 };
