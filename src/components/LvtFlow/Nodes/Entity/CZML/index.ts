89785a9a534ad6e9e3e8506e58612a09efe44280
lby <icesunex@hotmail.com>
2022-03-19 11:58:49 +0800

    remove former czml nodes

--- src/components/LvtFlow/Nodes/Entity/CZML/index.ts
+++ src/components/LvtFlow/Nodes/Entity/CZML/index.ts
@@ -1,22 +1,9 @@
-import BasicPacket from "./BasicPacket";
-import Billboard from "./Billboard";
 import Document from "./Document";
 import Merge from "./Merge";
 import Mix from "./Mix";
-import Point from "./Point";
-import Positions from "./Positions";
 import Renderer from "./Renderer";
 
 export default {
   _id: "CZML",
-  children: [
-    BasicPacket,
-    Billboard,
-    Point,
-    Positions,
-    Mix,
-    Merge,
-    Document,
-    Renderer,
-  ],
+  children: [Mix, Merge, Document, Renderer],
 };
