15a4bcad7f609b0a1b016662a5feeb667ea390a7
lby <icesunex@hotmail.com>
2022-03-20 15:23:25 +0800

    auto transfer to Number

--- src/components/Editor/NodeValueComponents/NumberInput.tsx
+++ src/components/Editor/NodeValueComponents/NumberInput.tsx
@@ -2,11 +2,12 @@ const NumberInput = ({ port, node, lvtFlow }: any) => {
   return (
     <div className="property-value ">
       <input
-        value={port.value.v}
+        value={port.value.v === undefined ? "" : port.value.v}
         disabled={port.connected}
         onChange={(e) => {
-          const number = e.target.value.replace(/[^\d.]/g, "");
-          port.setValue(number);
+          // const number = e.target.value.replace(/[^\d.]/g, "");
+          const number = e.target.value;
+          port.setValue(Number(number));
           lvtFlow.reRenderUI(["currentElement"]);
         }}
         onBlur={() => {
