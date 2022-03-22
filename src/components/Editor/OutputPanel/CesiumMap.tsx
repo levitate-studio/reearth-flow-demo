829e2f42f03cfa1a21c4b8562aac966989f6eba1
lby <icesunex@hotmail.com>
2022-03-22 19:48:15 +0800

    add init options and ui control

--- src/components/Editor/OutputPanel/CesiumMap.tsx
+++ src/components/Editor/OutputPanel/CesiumMap.tsx
@@ -1,5 +1,4 @@
-// import * as Cesium from "cesium";
-import { Viewer, CzmlDataSource, GeoJsonDataSource } from "cesium";
+import * as Cesium from "cesium";
 import { useEffect, useContext, useState } from "react";
 
 import { LvtFlowContext } from "../../../pages/Editor/index";
@@ -13,6 +12,9 @@ let dataSourcePromise: any;
 let rendering = false;
 let dataVersion = 0;
 let projectId = 0;
+let isNewProjectRendering: boolean;
+let renderData: any;
+let terrainProvider: string;
 
 const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
   // update control
@@ -24,10 +26,10 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
   // =======================================
   // rander data
   // only affect by viewer and data
-  const renderData = async () => {
+  const renderCzml = async () => {
     if (cesiumViewer) {
       rendering = true;
-      const renderData = lvtFlow.renderData?.v;
+      renderData = lvtFlow.renderData?.v;
 
       // remove old data
       clog.log("Cesium", `clearing data...`);
@@ -40,14 +42,20 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
       if (renderData) {
         const curDataVersion = lvtFlow.dataVersion;
         const curprojectId = lvtFlow.projectId;
+        isNewProjectRendering = projectId !== lvtFlow.projectId;
+        //
         clog.log("Cesium", `loading data: v[${curDataVersion}]`);
         switch (renderData.dataType) {
           case "CZML":
           default:
-            dataSourcePromise = await CzmlDataSource.load(renderData.data);
+            dataSourcePromise = await Cesium.CzmlDataSource.load(
+              renderData.data
+            );
             break;
           case "GeoJSON":
-            dataSourcePromise = await GeoJsonDataSource.load(renderData.data);
+            dataSourcePromise = await Cesium.GeoJsonDataSource.load(
+              renderData.data
+            );
             break;
         }
         const t2 = new Date().getTime();
@@ -55,6 +63,7 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
 
         // render new data
         cesiumViewer.dataSources.add(dataSourcePromise);
+
         const t3 = new Date().getTime();
         clog.log(
           "Cesium",
@@ -62,16 +71,13 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
         );
         dataVersion = curDataVersion;
         projectId = curprojectId;
+
+        onInit();
       } else {
         dataVersion = 0;
         projectId = 0;
       }
       cesiumViewer.scene.requestRender();
-
-      // auto focus
-      if (dataVersion === 1) {
-        focus();
-      }
 
       rendering = false;
     } else {
@@ -79,9 +85,53 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
     }
   };
 
+  const onInit = (isRest = false) => {
+    const ds = cesiumViewer.dataSources.get(0);
+    if (ds && (isNewProjectRendering || isRest)) {
+      isNewProjectRendering = false;
+      clog.log("Cesium", `apply init options`);
+
+      // focus
+      focus();
+
+      // trackentity
+      if (renderData?.options?.onInit?.trackedEntityId) {
+        cesiumViewer.trackedEntity = null;
+        cesiumViewer.trackedEntity = ds.entities.getById(
+          renderData?.options?.onInit?.trackedEntityId
+        );
+      }
+
+      // animate
+      if (renderData?.options?.onInit?.shouldAnimate) {
+        cesiumViewer.clock.currentTime = cesiumViewer.clock.startTime;
+        cesiumViewer.clock.shouldAnimate = true;
+      }
+
+      // terrainProvider
+      if (
+        renderData?.options?.onInit?.terrainProvider &&
+        terrainProvider !== renderData?.options?.onInit?.terrainProvider
+      ) {
+        terrainProvider = renderData?.options?.onInit?.terrainProvider;
+        switch (terrainProvider) {
+          case "createWorldTerrain":
+            cesiumViewer.terrainProvider = Cesium.createWorldTerrain();
+            break;
+          default:
+            cesiumViewer.terrainProvider =
+              new Cesium.EllipsoidTerrainProvider();
+            break;
+        }
+      }
+
+      setTimeout(() => {}, 0);
+    }
+  };
+
   const updateCesium = async () => {
     if (autoUpdate && !skipUpdate && !rendering) {
-      await renderData();
+      await renderCzml();
     }
   };
 
@@ -90,7 +140,7 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
   // =======================================
   useEffect(() => {
     const cesiumContainer = document.getElementById("cesium-container");
-    cesiumViewer = new Viewer(cesiumContainer as HTMLElement, {
+    cesiumViewer = new Cesium.Viewer(cesiumContainer as HTMLElement, {
       requestRenderMode: true,
       fullscreenElement: cesiumContainer as HTMLElement,
     });
@@ -109,8 +159,8 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
     // update UI
     const renderData = lvtFlow.renderData?.v;
     if (renderData) {
-      setAnimationUI(renderData.options?.showAnimation);
-      setTimelineUI(renderData.options?.showTimeline);
+      setAnimationUI(renderData.options?.showAnimation !== false);
+      setTimelineUI(renderData.options?.showTimeline !== false);
     }
     if (dataVersion != lvtFlow.dataVersion || projectId != lvtFlow.projectId) {
       updateCesium();
@@ -154,7 +204,7 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
         <div
           className="cesium-ui-ele button"
           onClick={() => {
-            renderData();
+            renderCzml();
           }}
         >
           UPDATE
@@ -166,6 +216,14 @@ const CesiumMap = ({ skipUpdate }: { skipUpdate: boolean }) => {
           }}
         >
           FOCUS
+        </div>
+        <div
+          className="cesium-ui-ele button"
+          onClick={() => {
+            onInit(true);
+          }}
+        >
+          RESET
         </div>
       </div>
     </>
