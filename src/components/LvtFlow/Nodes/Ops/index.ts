1d977de9f24229cb8b227877d3b693b69a90d7d4
lby <icesunex@hotmail.com>
2022-03-31 20:40:17 +0800

    remove category of Number and String

--- src/components/LvtFlow/Nodes/Ops/index.ts
+++ src/components/LvtFlow/Nodes/Ops/index.ts
@@ -1,18 +1,18 @@
 import BooleanSpread from "./BooleanSpread";
 import Color from "./Color";
-import Number from "./Number";
+// import Number from "./Number";
 import NumberSpread from "./NumberSpread";
 import ObjectSpread from "./ObjectSpread";
-import String from "./String";
+// import String from "./String";
 import StringSpread from "./StringSpread";
 
 export default {
   _id: "Ops",
   children: [
     BooleanSpread,
-    Number,
+    // Number,
     NumberSpread,
-    String,
+    // String,
     StringSpread,
     ObjectSpread,
     Color,
