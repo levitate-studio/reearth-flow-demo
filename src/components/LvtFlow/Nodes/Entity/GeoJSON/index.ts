2e78a0c981190bb5c0dfca2e91d8230c83f2417e
lby <icesunex@hotmail.com>
2022-04-12 16:50:28 +0800

    add node

--- src/components/LvtFlow/Nodes/Entity/GeoJSON/index.ts
+++ src/components/LvtFlow/Nodes/Entity/GeoJSON/index.ts
@@ -1,9 +1,10 @@
 import Document from "./Document";
+import MergeFeatures from "./MergeFeatures";
 import MultiPoint from "./MultiPoint";
 import RandomPoint from "./RandomPoint";
 import Renderer from "./Renderer";
 
 export default {
   _id: "GeoJSON",
-  children: [Document, RandomPoint, MultiPoint, Renderer],
+  children: [Document, RandomPoint, MultiPoint, Renderer, MergeFeatures],
 };
