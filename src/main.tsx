3d46459ba6f71f60bf0e7a9f0332a4d68d420849
lby <icesunex@hotmail.com>
2022-09-30 22:25:21 +0800

    chore: upgrade dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(<App />);
