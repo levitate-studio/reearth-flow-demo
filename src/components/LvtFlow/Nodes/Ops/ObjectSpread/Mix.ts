99929e374b0f83ffbc55c4537a5fd97c41d749c9
lby <icesunex@hotmail.com>
2022-10-01 11:29:25 +0800

    feat: extend mix with more input port

--- src/components/LvtFlow/Nodes/Ops/ObjectSpread/Mix.ts
+++ src/components/LvtFlow/Nodes/Ops/ObjectSpread/Mix.ts
@@ -18,6 +18,16 @@ const Mix: LvtNodeDef = {
       dataType: "objectSpread",
       defaultValue: {},
     },
+    {
+      name: "objects3",
+      dataType: "objectSpread",
+      defaultValue: {},
+    },
+    {
+      name: "objects4",
+      dataType: "objectSpread",
+      defaultValue: {},
+    },
   ],
   portsOut: [
     {
@@ -25,21 +35,30 @@ const Mix: LvtNodeDef = {
       dataType: "objectSpread",
     },
   ],
-  rule: (objects1: any, objects2: any) => {
+  rule: (objects1: any, objects2: any, objects3: any, objects4: any) => {
     const _objects1 = spreadData(objects1);
     const _objects2 = spreadData(objects2);
-    const max = Math.max(_objects1.length, _objects2.length);
+    const _objects3 = spreadData(objects3);
+    const _objects4 = spreadData(objects4);
+    const max = Math.max(
+      _objects1.length,
+      _objects2.length,
+      _objects3.length,
+      _objects4.length
+    );
     const _temp = [];
     for (let i = 0; i < max; i += 1) {
       _temp.push({
         ..._objects1[i % _objects1.length],
         ..._objects2[i % _objects2.length],
+        ..._objects3[i % _objects3.length],
+        ..._objects4[i % _objects4.length],
       });
     }
     return _temp;
   },
   update: (node: LvtNode) => {
-    updateNode(node, "mixed", ["objects1", "objects2"]);
+    updateNode(node, "mixed", ["objects1", "objects2", "objects3", "objects4"]);
     return node;
   },
 };
